c3997fb3e30fc8763d4e68b3eb408f330ca2e6fd
aldelaro5 <aldelaro5@gmail,com>
2021-03-18 03:01:23 -0400

    Change the exception thrown on convert back for the enum description converter

--- Common/EnumToDescriptionConverter.cs
+++ Common/EnumToDescriptionConverter.cs
@@ -24,7 +24,7 @@ namespace BugFablesSaveEditor
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-      throw new NotImplementedException();
+      throw new NotSupportedException();
     }
   }
 }
