d132d01006b6c3c2cee0e493a1199e7ed0335a81
aldelaro5 <aldelaro5@gmail.com>
2023-03-05 01:47:37 -0500

    Fix warnings in ViewLocator

--- Source/ViewLocator.cs
+++ Source/ViewLocator.cs
@@ -1,29 +1,23 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
-using BugFablesSaveEditor.ViewModels;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace BugFablesSaveEditor;
 
 public class ViewLocator : IDataTemplate
 {
-  public bool SupportsRecycling => false;
-
-  public Control Build(object data)
+  public Control Build(object? data)
   {
-    string name = data.GetType().FullName!.Replace("ViewModel", "View");
-    Type? type = Type.GetType(name);
-
+    string? name = data?.GetType().FullName!.Replace("ViewModel", "View");
+    Type? type = name is not null ? Type.GetType(name) : null;
     if (type != null)
-    {
       return (Control)Activator.CreateInstance(type)!;
-    }
 
     return new TextBlock { Text = "Not Found: " + name };
   }
 
-  public bool Match(object data)
+  public bool Match(object? data)
   {
     return data is ObservableObject;
   }
