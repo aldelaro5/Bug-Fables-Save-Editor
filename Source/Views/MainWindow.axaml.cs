4e84387bf754f9b6ee7b8c47535910ca7810efc9
aldelaro5 <aldelaro5@gmail.com>
2023-03-10 01:06:26 -0500

    Remove settings code

--- Source/Views/MainWindow.axaml.cs
+++ Source/Views/MainWindow.axaml.cs
@@ -14,8 +14,6 @@ public partial class MainWindow : Window
   {
     InitializeComponent();
 
-    SettingsManager.Load();
-
     Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
     Trace.AutoFlush = true;
   }
@@ -24,21 +22,5 @@ public partial class MainWindow : Window
   {
     AboutView view = new() { Width = 500, Height = 350 };
     await view.ShowDialog(Common.MainWindow);
-  }
-
-  public async void OnLoaded(object sender, EventArgs e)
-  {
-    if (SettingsManager.Settings.ShowStartupWarning)
-    {
-      var msg = MessageBoxManager.GetMessageBoxStandardWindow("Warning",
-        "This save editor is in beta and thus, " +
-        "may lead to data losses due to instability.\n" +
-        "It is HIGHLY recommended to backup your save files before usage.",
-        ButtonEnum.Ok, MessageBox.Avalonia.Enums.Icon.Warning);
-      await msg.ShowDialog(Common.MainWindow);
-
-      SettingsManager.Settings.ShowStartupWarning = false;
-      SettingsManager.Save();
-    }
   }
 }
