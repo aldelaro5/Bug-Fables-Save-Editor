f18586873bb5083f64005827c5b15fec300b099a
aldelaro5 <aldelaro5@gmail.com>
2023-04-18 05:35:02 -0400

    Rework the toplevel getter

--- BugFablesSaveEditor/BugFablesSaveEditor/Utils.cs
+++ BugFablesSaveEditor/BugFablesSaveEditor/Utils.cs
@@ -1,31 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq.Expressions;
-using System.Reactive.Linq;
-using System.Threading;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
-using BugFablesSaveEditor.Models;
-using DynamicData;
-using DynamicData.Binding;
 
 namespace BugFablesSaveEditor;
 
 public static class Utils
 {
-  public static Window MainWindow
+  public static TopLevel TopLevel
   {
     get
     {
       var lifetime = Application.Current?.ApplicationLifetime!;
       if (lifetime is IClassicDesktopStyleApplicationLifetime)
         return ((IClassicDesktopStyleApplicationLifetime)lifetime).MainWindow!;
-      // TODO: Figure out how to do dialogs on browser
-      // if (lifetime is ISingleViewApplicationLifetime singleViewPlatform)
-      //   return ((ISingleViewApplicationLifetime)lifetime).MainView;
+      if (lifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        return TopLevel.GetTopLevel(singleViewPlatform.MainView)!;
       return null!;
     }
   }
