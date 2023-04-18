54d0b7dc22e95df8e8f5a29547184a91c52e407a
aldelaro5 <aldelaro5@gmail.com>
2023-04-18 18:36:08 -0400

    Fix the fonts not loading issue

--- BugFablesSaveEditor/BugFablesSaveEditor.Browser/Program.cs
+++ BugFablesSaveEditor/BugFablesSaveEditor.Browser/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.Versioning;
 using Avalonia;
 using Avalonia.Browser;
+using Avalonia.Fonts.Inter;
 using BugFablesSaveEditor;
 using Projektanker.Icons.Avalonia;
 using Projektanker.Icons.Avalonia.MaterialDesign;
@@ -15,6 +16,7 @@ internal partial class Program
   public static AppBuilder BuildAvaloniaApp()
     => AppBuilder
       .Configure<App>()
+      .WithInterFont()
       .WithIcons(container => container
         .Register<MaterialDesignIconProvider>());
 }
