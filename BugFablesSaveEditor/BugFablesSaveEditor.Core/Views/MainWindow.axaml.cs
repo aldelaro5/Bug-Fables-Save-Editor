15872fc8b3e2d8da1d5b2d9f307c7136ab97f42e
aldelaro5 <aldelaro5@gmail.com>
2023-05-11 01:57:15 -0400

    Upgrade Avalonia to 11.0 preview8

--- BugFablesSaveEditor/BugFablesSaveEditor.Core/Views/MainWindow.axaml.cs
+++ BugFablesSaveEditor/BugFablesSaveEditor.Core/Views/MainWindow.axaml.cs
@@ -8,8 +8,4 @@ public partial class MainWindow : Window
   {
     InitializeComponent();
   }
-
-  // Workaround for https://github.com/AvaloniaUI/Avalonia/issues/11149
-  // TODO: Remove this hack when we upgrade to any version past preview7 as the fix got merged
-  private void Window_OnClosing(object? sender, WindowClosingEventArgs e) => MainView.DataContext = null;
 }
