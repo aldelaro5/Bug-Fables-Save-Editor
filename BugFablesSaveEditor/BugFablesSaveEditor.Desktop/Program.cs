d0fc2e648af1426e2eb3700b3d0e5a19854df7aa
aldelaro5 <aldelaro5@gmail.com>
2023-06-26 19:08:37 -0400

    Upgrade to avalonia 11rc1

--- BugFablesSaveEditor/BugFablesSaveEditor.Desktop/Program.cs
+++ BugFablesSaveEditor/BugFablesSaveEditor.Desktop/Program.cs
@@ -17,9 +17,11 @@ class Program
 
   // Avalonia configuration, don't remove; also used by visual designer.
   public static AppBuilder BuildAvaloniaApp()
-    => AppBuilder.Configure<App>()
+  {
+    IconProvider.Current.Register<MaterialDesignIconProvider>();
+
+    return AppBuilder.Configure<App>()
       .UsePlatformDetect()
-      .LogToTrace()
-      .WithIcons(container => container
-        .Register<MaterialDesignIconProvider>());
+      .LogToTrace();
+  }
 }
